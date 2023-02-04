d59590995622ab51689873fd4e8b20ba613074d6
NewGyu <newgyu+git@kinoboku.net>
2023-02-04 14:51:14 +0000

    remove unused

--- src/codes/Repository/CharacterRepo.ts
+++
@@ -1,6 +0,0 @@
-import type { Character } from "../Entities";
-import { loadYAML } from "./YamlUtil";
-
-export function listAll(): Character[] {
-    return loadYAML<Character[]>("data/Character.yaml");
-}
