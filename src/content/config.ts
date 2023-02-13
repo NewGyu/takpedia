cd4873c99c30b45392d82c7c83e0d5320e9445b3
NewGyu <newgyu+git@kinoboku.net>
2023-02-13 16:06:44 +0000

    Collection名の登録漏れ

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,9 @@
 import { z, defineCollection } from "astro:content";
-import { PersonSchema, TeamSchema } from "./schemas";
+import { PersonSchema, TeamSchema, FamousQuoteSchema } from "./schemas";
 
 
 export const collections = {
     "people": PersonSchema,
     "teams": TeamSchema,
+    "famousQuotes": FamousQuoteSchema,
 }
