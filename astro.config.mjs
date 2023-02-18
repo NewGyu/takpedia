20ecfa94fc9bbc56dcea0f58b24adf765309d404
NewGyu <newgyu+git@kinoboku.net>
2023-02-18 14:48:08 +0000

    install pwa astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bukko.me/",
-  integrations: [tailwind(), mdx(), sitemap(), react()]
+  integrations: [tailwind(), mdx(), sitemap(), react(), AstroPWA()]
 });
