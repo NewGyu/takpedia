22c6a907bb81c2efa49c10e53c7566ac804b54a5
NewGyu <newgyu+git@kinoboku.net>
2023-02-19 14:16:09 +0000

    Revert "Merge pull request #31 from NewGyu/feature/pwa"
    This reverts commit 6bd6d8fc8ee5d1216ca6822cbeca5cc15b95c8a3, reversing
    changes made to a6de850163635990a12ebc4bce9084fa221f5a12.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,16 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import AstroPWA from "@vite-pwa/astro";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bukko.me/",
-  integrations: [
-    tailwind(), mdx(), sitemap(), react(),
-    AstroPWA({
-      manifest: {
-        name: "特攻の拓総合サイト拓ペディア",
-        short_name: "Takpedia",
-        icons: [{
-          src: "icon/taku-icon.png",
-          sizes: "256x256",
-          type: "image/png"
-        }]
-      }
-    })
-  ]
+  integrations: [tailwind(), mdx(), sitemap(), react()]
 });
