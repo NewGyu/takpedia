08e4d6559c8238c2d609af74c74832177c10a2e4
NewGyu <newgyu+git@kinoboku.net>
2023-02-20 15:16:27 +0000

    adjust pwa configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,11 +16,21 @@ export default defineConfig({
       manifest: {
         name: "特攻の拓総合サイト拓ペディア",
         short_name: "Takpedia",
+        lang: "ja",
+        background_color: "#0573b2",
         icons: [{
           src: "icon/taku-icon.png",
           sizes: "256x256",
           type: "image/png"
         }]
+      },
+      workbox: {
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+        navigateFallback: "/404"
       }
     })
   ]
