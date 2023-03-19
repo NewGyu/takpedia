61df55dc579deb003e25e04f275dbaf372873f78
NewGyu <newgyu+git@kinoboku.net>
2023-03-19 09:34:04 +0000

    remove darkmode

--- src/components/Gpt/Input.tsx
+++ src/components/Gpt/Input.tsx
@@ -15,7 +15,7 @@ export const InputArea = ({ onCommented }: Props) => {
     };
 
     return (
-        <div className="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
+        <div className="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)]">
             <TextArea comment={newComment} valueChanged={(v) => setComment(v)} />
             <Button onClick={handleButtonClick} />
         </div >
@@ -38,7 +38,7 @@ const TextArea = ({ comment, valueChanged }: TextAreaProps) => {
         <textarea
             style={css}
             rows={1}
-            className="m-0 w-full resize-none border-0 bg-transparent p-0 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent pl-2 md:pl-0"
+            className="m-0 w-full resize-none border-0 bg-transparent p-0 pr-7 focus:ring-0 focus-visible:ring-0 pl-2 md:pl-0"
             value={comment}
             onChange={e => { valueChanged(e.target.value) }}
         />
@@ -51,7 +51,7 @@ interface ButtonProps {
 const Button = ({ onClick }: ButtonProps) => {
     return (
         <button
-            className="absolute p-1 rounded-md text-gray-500 bottom-1.5 md:bottom-2.5 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent right-1 md:right-2"
+            className="absolute p-1 rounded-md text-gray-500 bottom-1.5 md:bottom-2.5 hover:bg-gray-100 disabled:hover:bg-transparent right-1 md:right-2"
             onClick={onClick}
         >
             <svg stroke="currentColor"
